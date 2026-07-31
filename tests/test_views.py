59fca8e7490214522242352ccd61df2800b6bc4b
Robert Streetman <rob.git@mailfence.com>
2026-07-30 19:35:13 -0600

    Split remaining view tests.

--- tests/test_views.py
+++ tests/test_views.py
@@ -5,29 +5,25 @@ def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
 
-    response = client.get('/index')
-    assert response.status_code == 200
-
-    response = client.get('/main')
-    assert response.status_code == 200
-
-    response = client.get('/home')
-    assert response.status_code == 200
-
 '''
 Test Main Menu content
 '''
 def test_index_content(client):
     response = client.get('/')
-    assert b'<title>Pyle - Main</title>' and b'<h1>Main Menu</h1>' in response.data
+    assert b'<h1>Main Menu</h1>' in response.data
 
 '''
 Test if Vehicle Details page is loading
 '''
 def test_details(client):
     response = client.get('/details')
-
     assert response.status_code == 200
+
+'''
+Test Vehicle Details page content
+'''
+def test_details_content(client):
+    response = client.get('/details')
     assert b'<h1>Vehicle Details</h1>' in response.data
 
 '''
@@ -35,8 +31,14 @@ Test if Vehicle History page is loading
 '''
 def test_history(client):
     response = client.get('/history')
-
     assert response.status_code == 200
+    assert b'<h1>Maintenance History</h1>' in response.data
+
+'''
+Test Vehicle History content
+'''
+def test_history_content(client):
+    response = client.get('/history')
     assert b'<h1>Maintenance History</h1>' in response.data
 
 '''
@@ -47,5 +49,12 @@ def test_status(client):
 
     assert response.status_code == 200
     
+    assert b'<h1>Vehicle Status</h1>' in response.data
+
+'''
+Test Vehicle Status content
+'''
+def test_status_content(client):
+    response = client.get('/status')
     assert b'<h1>Vehicle Status</h1>' in response.data
 
