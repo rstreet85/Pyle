0663a45b5b88ab953f453772a9d04d4c81a8f292
Robert Streetman <robert.streetman.engineer@gmail.com>
2026-08-11 23:02:32 -0600

    Added form for new record. Fixed formatting in forms.py. Fixed some variables names in templates.

--- src/Pyle/forms.py
+++ src/Pyle/forms.py
@@ -2,13 +2,16 @@ from decimal import Decimal
 
 from flask_wtf import FlaskForm
 from wtforms import (
+    DateTimeLocalField,
     DecimalField,
     IntegerField,
     StringField,
-    SubmitField
+    SubmitField,
+    TextAreaField
     )
 from wtforms.validators import (
     DataRequired,
+    Length,
     NumberRange
     )
 
@@ -36,6 +39,41 @@ Form to select new active vehicle
 '''
 
 '''
-Form to add maintenance event
+Form to add maintenance Record
 '''
-
+class NewRecordForm(FlaskForm):
+    vehicle_name = StringField(
+        'Vehicle Name',
+        validators=[DataRequired(message='Must provide profile name.')]
+        )
+    date = DateTimeLocalField(
+        'Date/Time',
+        validators=[DataRequired(message='Must add the date.')]
+        )
+    mileage = DecimalField(
+        'Miles',
+        validators=[NumberRange(min=Decimal('0.01'), message='Mileage cannot be negative.')]
+        )
+    technician = StringField(
+        'Technician',
+        validators=[DataRequired(message='Must provide technician name.')]
+        )
+    service_name = StringField(
+        'Service Name',
+        validators=[DataRequired(message='Must provide service name.')]
+        )
+    service_notes = TextAreaField(
+        'Service Notes',
+        validators=[Length(max=500, message='Note is too long.')]
+        )
+    replacement_part = StringField('Replacement Part')
+    lifespan_miles = IntegerField(
+        'Lifespan (Miles)',
+        validators=[NumberRange(min=0, message='Mileage cannot be negative.')]
+        )
+    lifespan_months = IntegerField(
+            'Lifespan (Months)',
+            validators=[NumberRange(min=0, message='Months cannot be negative.')]
+            )
+    
+    add_record = SubmitField('Save Record')
